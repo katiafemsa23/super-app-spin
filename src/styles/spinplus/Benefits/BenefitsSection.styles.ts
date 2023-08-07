9bef23a79a93572d27e65b68bb03caf785026be7
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-08-07 14:59:06 -0700

    Adding BenefitsSection component with related styles

---
+++ src/styles/spinplus/Benefits/BenefitsSection.styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    //paddingVertical: 24,
+  },
+  title: {
+    fontWeight: '500',
+    lineHeight: 24,
+  },
+  paragraph: {
+    marginVertical: 12,
+    lineHeight: 20,
+    letterSpacing: -0.32,
+    fontWeight: '400',
+  },
+  img: {
+    alignSelf: 'center',
+  },
+});
+
+export default styles;
