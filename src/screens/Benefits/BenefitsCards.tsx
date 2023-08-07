9da94fe6cadeecf85aed9b9484023e24773cd7a1
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-08-07 12:53:38 -0700

    BenefitsCards component, BenefitsCard component  created with the necessary styles

---
+++ src/screens/Benefits/BenefitsCards.tsx
@@ -0,0 +1,44 @@
+import { View, Image, ImageProps } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import styles from '../../styles/spinplus/Benefits/BenefitsCards';
+import Card from '../../components/Card/Card';
+import Text from '../../components/Text/Text';
+
+type BenefitCardT = {
+  text: string;
+  img: ImageProps;
+  onPress: () => void;
+};
+
+// TODO: Add correct navigation on onPress action of the card when Movimientos and Cambio de puntos screen is ready
+const BenefitCard = ({ img, text, onPress }: BenefitCardT) => {
+  return (
+    <Card style={styles.cardContainer}>
+      <TouchableOpacity>
+        <Image width={124} height={124} source={img} />
+        <View style={styles.cardTextContainer}>
+          <Text variant="small-body-bold" style={styles.cardText}>
+            {text}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </Card>
+  );
+};
+
+export const BenefitsCards = () => {
+  return (
+    <View style={styles.container}>
+      <BenefitCard
+        onPress={() => {}}
+        text={`Consulta \n tu historial`}
+        img={require('../../assets/benefits-history.png')}
+      />
+      <BenefitCard
+        onPress={() => {}}
+        text="Cambia tus puntos"
+        img={require('../../assets/change-points.png')}
+      />
+    </View>
+  );
+};
---
+++ src/styles/spinplus/Benefits/BenefitsCards.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 16,
+    paddingVertical: 16,
+    flexDirection: 'row',
+  },
+  cardContainer: {
+    height: 204,
+  },
+  cardTextContainer: {
+    padding: 12,
+  },
+  cardText: {
+    lineHeight: 20,
+    fontWeight: '600',
+    textAlign: 'center',
+    letterSpacing: -0.28,
+  },
+});
+
+export default styles;
