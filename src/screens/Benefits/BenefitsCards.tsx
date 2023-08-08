027046dbc02819af6cf7b3eaa1ba634f6d4820fa
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-08-07 17:51:54 -0700

    Fixing comments

--- src/screens/Benefits/BenefitsCards.tsx
+++ src/screens/Benefits/BenefitsCards.tsx
@@ -7,11 +7,13 @@ import styles from '../../styles/spinplus/Benefits/BenefitsCards';
 type BenefitCardT = {
   text: string;
   img: ImageProps;
-  onPress: () => void;
+  // REPLACE FOR SCREENS TYPE
+  navigateTo: string;
 };
 
 // TODO: Add correct navigation on onPress action of the card when Movimientos and Cambio de puntos screen is ready
-const BenefitCard = ({ img, text, onPress }: BenefitCardT) => {
+const BenefitCard = ({ img, text, navigateTo }: BenefitCardT) => {
+  // ADD NAVIGATION HOOK WHEN MERGED NAVIGATION
   return (
     <Card style={styles.cardContainer} testID="benefit-card">
       <TouchableOpacity>
@@ -30,12 +32,12 @@ export const BenefitsCards = () => {
   return (
     <View style={styles.container} testID="benefit-cards">
       <BenefitCard
-        onPress={() => {}}
+        navigateTo=""
         text={`Consulta \n tu historial`}
         img={require('../../assets/benefits-history.png')}
       />
       <BenefitCard
-        onPress={() => {}}
+        navigateTo=""
         text="Cambia tus puntos"
         img={require('../../assets/change-points.png')}
       />
