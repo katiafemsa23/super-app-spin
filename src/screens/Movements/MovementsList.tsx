d35ca418feba5d0ddbf60ad8d86d060b14308d2b
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-08-09 10:28:35 -0700

    Breaking components on MovementsDetail

--- src/screens/Movements/MovementsList.tsx
+++ src/screens/Movements/MovementsList.tsx
@@ -53,42 +53,7 @@ export const MovementList = ({ data, isLoading }: PropsT) => {
         <Spinner />
       ) : (
         <FlatList
-          data={[
-            {
-              entity: 'Oxxo Gas',
-              date: 'Sun Aug 06 2023',
-              points: 100,
-              operation: 'earned',
-              transactionNo: '5dced89c-2b6e-4a1c-a715-c19b0a51',
-              id: 1,
-            },
-            {
-              entity: 'Volaris',
-              date: 'Sun Aug 01 2023',
-              points: 1000,
-              operation: 'earned',
-              transactionNo: '5dced89c-2b6e-4a1c-a715-c19b0a51',
-              id: 2,
-            },
-            {
-              entity: 'Volaris',
-              date: 'Sun Aug 01 2023',
-              points: 1000,
-              operation: 'earned',
-              transactionNo: '5dced89c-2b6e-4a1c-a715-c19b0a51',
-              id: 3,
-            },
-            {
-              entity: 'Volaris',
-              date: 'Sun Aug 01 2023',
-              expiryDate: 'Fri Sep 01 2023',
-              points: 1000,
-              operation: 'earned',
-              transactionNo: '5dced89c-2b6e-4a1c-a715-c19b0a51',
-              giftCode: '42738499092812000',
-              id: 4,
-            },
-          ]}
+          data={data}
           renderItem={renderItem}
           keyExtractor={(item, index) => `item-${item.id}${index}`}
         />
--- src/screens/MovementsDetail/MovementsDetail.tsx
+++ src/screens/MovementsDetail/MovementsDetail.tsx
@@ -1,12 +1,10 @@
-import React, { useMemo } from 'react';
-import { Image, View, SafeAreaView, useWindowDimensions } from 'react-native';
-import { formatDate } from '../../utils';
+import React from 'react';
+import { View, SafeAreaView } from 'react-native';
 import useTheme from '../../hooks/useTheme';
 import Text from '../../components/Text/Text';
 import styles from '../../styles/spinplus/MovementsDetail.styles';
-import Card from '../../components/Card/Card';
-
-const placeholderImage = require('../../assets/oxxo.png');
+import { MovementCard } from './MovementCard';
+import { MovementDetailSection } from './MovementDetailSection';
 
 type MovementsDetailProps = {
   item: Omit<HistoryItem, 'id' | 'operation'>;
@@ -16,59 +14,13 @@ const MovementsDetail = ({
   item: { date, entity, points, transactionNo },
 }: MovementsDetailProps) => {
   const theme = useTheme();
-  const dimensions = useWindowDimensions();
-  const positivePoints = points >= 0;
-  const formattedDate = useMemo(() => formatDate(date), [date]);
 
   return (
     <SafeAreaView
       style={{ backgroundColor: theme.colors.surface_primary, flex: 1 }}>
       <View style={styles.firstSectionContainer}>
-        <Card style={styles.cardContainer}>
-          <View style={styles.imageContainer}>
-            <Image source={placeholderImage} style={styles.image} />
-          </View>
-          <View style={styles.entityContainer}>
-            <Text variant="headline-medium" style={styles.entityText}>
-              {entity}
-            </Text>
-            <Text
-              variant="default-body"
-              style={styles.entityCardSubtitle}>{`En esta compra ${
-              positivePoints ? 'ganaste' : 'usaste'
-            }:`}</Text>
-            <View style={styles.pointsContainer}>
-              <Text variant="headline-large" style={styles.symbol}>
-                {positivePoints ? '+' : '-'}
-              </Text>
-              <Text
-                variant="headline-extra-large"
-                style={{ fontWeight: '500' }}>
-                {points}
-              </Text>
-            </View>
-          </View>
-        </Card>
-        {positivePoints && (
-          <View style={styles.valueLabelContainer}>
-            <Text variant="label-default">Monto total:</Text>
-            <Text variant="default-body-bold" style={styles.value}>
-              ${(points * 10).toFixed(2)}
-            </Text>
-          </View>
-        )}
-        <View style={styles.valueLabelContainer}>
-          <Text variant="label-default">Fecha:</Text>
-          <Text variant="default-body-bold" style={styles.value}>
-            {formattedDate}
-          </Text>
-        </View>
-        <View style={styles.valueLabelContainer}>
-          <Text variant="label-default">Úsalos desde el:</Text>
-          <Text variant="default-body-bold" style={styles.value}>
-            {formattedDate}
-          </Text>
-        </View>
+        <MovementCard entity={entity} points={points} />
+        <MovementDetailSection date={date} points={points} />
       </View>
       <View style={styles.transactionContainer}>
         <Text variant="label-default">Número de transacción</Text>
