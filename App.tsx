d2f1b08e800ac58c23cbe66ef2c053acced7eec6
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-08-10 12:26:10 -0700

    Fixing Header cutting off on bigger devices

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import MainStackNavigator from './src/navigation/MainStackNavigator';
 import { HistorytProvider } from './src/history/historyContext';
 import ThemeProvider from './src/theme/ThemeProvider';
@@ -9,15 +10,17 @@ import { BottomSheet, SnackBar } from './src';
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <HistorytProvider>
-        <NavigationContainer>
-          <MainStackNavigator />
-        </NavigationContainer>
-        <SnackBar.Component />
-        <BottomSheet.Component />
-      </HistorytProvider>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <HistorytProvider>
+          <NavigationContainer>
+            <MainStackNavigator />
+          </NavigationContainer>
+          <SnackBar.Component />
+          <BottomSheet.Component />
+        </HistorytProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 };
 
